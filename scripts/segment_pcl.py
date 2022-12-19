c25f1551efb29487809a194f326d00a02a46fe56
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2022-12-19 02:54:16 +0100

    segmented objects on plane

--- scripts/segment_pcl.py
+++ scripts/segment_pcl.py
@@ -3,6 +3,9 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 from open3d_ros_helper import open3d_ros_helper as orh
 import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+# from perception.laptop_perception_helpers import RealsenseHelpers, constrain_environment
 
 
 class SegmentPCL:
@@ -12,8 +15,23 @@ class SegmentPCL:
   
   def pcl_callback(self, msg):
     rospy.loginfo("Received PointCloud2 message")
-    o3dpc = orh.rospc_to_o3dpc(msg) 
-    o3d.visualization.draw_geometries([o3dpc])
+    pcd = orh.rospc_to_o3dpc(msg) 
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+    print(plane_model)
+    plane_cloud = pcd.select_by_index(inliers)
+    plane_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=16), fast_normal_computation=True)
+    objects_cloud = pcd.select_by_index(inliers, invert=True)
+    plane_cloud.paint_uniform_color([1, 0, 0])
+    # outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+    # o3d.visualization.draw_geometries([plane_cloud, objects_cloud])
+    labels = np.array(objects_cloud.cluster_dbscan(eps=0.005, min_points=5))
+    max_label = labels.max()
+    colors = plt.get_cmap("tab20")(labels / (max_label 
+    if max_label > 0 else 1))
+    colors[labels < 0] = 0
+    objects_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    o3d.visualization.draw_geometries([objects_cloud])
+    print(objects_cloud)
 
 
 if __name__ == '__main__':
