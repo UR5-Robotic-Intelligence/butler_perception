5c191363206720b768ab362a961bd2058a7d6dcc
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2022-12-27 00:03:46 +0100

    constrain wrt aruco_base

--- scripts/segment_pcl.py
+++ scripts/segment_pcl.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import PointCloud2, Image
+from std_msgs.msg import String
 from open3d_ros_helper import open3d_ros_helper as orh
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from PIL import Image as PILImage
-from perception.laptop_perception_helpers import RealsenseHelpers
+from perception.laptop_perception_helpers import RealsenseHelpers, transform_dist_mat
+from robot_helpers.srv import LookupTransform, TransformPoses
 import cv2
 from ros_numpy import numpify
 import clip
@@ -29,20 +31,26 @@ class PCLProcessor:
     rospy.loginfo("Received PointCloud2 message")
     pcd = orh.rospc_to_o3dpc(msg) 
     np_points = np.asarray(pcd.points)
-    x_cond = np.logical_and(np_points[:, 0] >= 1.58-2, np_points[:, 0] <= 2.212-2)
-    z_cond = np_points[:, 2] <= 1.36
-    filtered_np_points = np.where(np.logical_and(x_cond, z_cond))
+    dist_mat = np_points.T
+    dist_mat = transform_dist_mat(dist_mat, 'camera_color_optical_frame', 'aruco_base')
+    np_points = dist_mat.T
+    limits = {'x_min': 0.46799999999999997, 'x_max': 1.157, 'y_min': -0.08899999999999997, 'y_max': 2.0, 'z_min': -0.7050000000000001, 'z_max': 0.6760000000000002}
+    x_cond = np.logical_and(np_points[:, 0] >= limits["x_min"], np_points[:, 0] <= limits["x_max"])
+    y_cond = np.logical_and(np_points[:, 1] >= limits["y_min"], np_points[:, 1] <= limits["y_max"])
+    z_cond = np.logical_and(np_points[:, 2] >= limits["z_min"], np_points[:, 2] <= limits["z_max"])
+    filtered_np_points = np.where(np.logical_and(x_cond, np.logical_and(y_cond, z_cond)))
     pcd.points = o3d.utility.Vector3dVector(np_points[filtered_np_points])
-    plane_model, inliers = pcd.segment_plane(distance_threshold=0.0047, ransac_n=3, num_iterations=1000)
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.025, ransac_n=3, num_iterations=1000)
     # print(plane_model)
     plane_cloud = pcd.select_by_index(inliers)
     # plane_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=16), fast_normal_computation=True)
     objects_cloud = pcd.select_by_index(inliers, invert=True)
     plane_cloud.paint_uniform_color([1, 0, 0])
     # outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+    # objects_cloud = pcd
     if visualize:
-      o3d.visualization.draw_geometries([plane_cloud, objects_cloud])
-    labels = np.array(objects_cloud.cluster_dbscan(eps=0.008, min_points=5))
+      o3d.visualization.draw_geometries([objects_cloud])
+    labels = np.array(objects_cloud.cluster_dbscan(eps=0.01, min_points=10))
     max_label = labels.max()
     colors = plt.get_cmap("tab20")(labels / (max_label 
     if max_label > 0 else 1))
@@ -53,36 +61,56 @@ class PCLProcessor:
     objects_boundaries = []
     for i in range(max_label+1):
       label_indices = np.where(labels == i)[0]
-      if len(label_indices) < 60:
+      if len(label_indices) < 80:
         continue
       cluster = objects_cloud.select_by_index(label_indices)
       if visualize:
         o3d.visualization.draw_geometries([cluster])
       points = np.asarray(cluster.points).T
+      points = transform_dist_mat(points, 'aruco_base', 'camera_color_optical_frame')
       intrinsics = self.rs_helpers.get_intrinsics(self.rs_helpers.color_intrin_topic)
       extrinsics = self.rs_helpers.get_depth_to_color_extrinsics()
+      extrinsics = None
       pixels = self.rs_helpers.calculate_pixels_from_points(points, intrinsics, cam_to_cam_extrinsics=extrinsics)
-      objects_boundaries.append([(min(pixels[1]), min(pixels[0])), (max(pixels[1]), max(pixels[0]))])
+      # print(pixels)
+      # pixels = np.round(pixels).astype(np.uint16)
       msg = rospy.wait_for_message("/camera/color/image_raw", Image)
       image_np = numpify(msg)
       image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+      pixels = self.rs_helpers.adjust_pixels_to_boundary(
+      pixels, (image_np.shape[1], image_np.shape[0]))
+      rh = 0
+      rv_up = 10
+      rv_down = 10
+      miny, minx = min(pixels[1])-rv_up, min(pixels[0])-rh
+      maxy, maxx = max(pixels[1])+rv_down, max(pixels[0])+rh
+      # boundary_pixels = self.rs_helpers.adjust_pixels_to_boundary(
+      # np.array([[minx, miny],[maxx, maxy]]), (image_np.shape[1], image_np.shape[0]))
+      # maxx, maxy = boundary_pixels[1]
+      # minx, miny = boundary_pixels[0]
+      if maxx - minx < 5 or maxy - miny < 5:
+        continue
+      objects_boundaries.append([(minx, miny),
+                                 (maxx, maxy)])
       if visualize:
-        cv2.imshow("image", image_np[min(pixels[1]):max(pixels[1]), min(pixels[0]):max(pixels[0])])
-        cv2.waitKey(0)
+        print(objects_boundaries[-1])
+        print(image_np.shape)
+        print(minx, miny, maxx, maxy)
+        cv2.imshow("image", image_np[miny:maxy, minx:maxx])
+        val = cv2.waitKey(0) & 0xFF
     return objects_boundaries, image_np
   
   def find_object(self, object_names):
-      objects_on_table_roi, image_np = self.segment_pcl(visualize=False)
+      objects_on_table_roi, image_np = self.segment_pcl(visualize=True)
       # image = PILImage.fromarray(np.uint8(image_np)*255)
       objects_images = []
       for object_roi in objects_on_table_roi:
-          obj_image = PILImage.fromarray(image_np[object_roi[0][0]-10:object_roi[1][0]+10, object_roi[0][1]-10:object_roi[1][1]+10])
-          cv2.imshow("image", np.array(obj_image))
+          obj_image = PILImage.fromarray(image_np[object_roi[0][1]:object_roi[1][1], object_roi[0][0]:object_roi[1][0]])
+          cv2.imshow("object_image", np.array(obj_image))
           cv2.waitKey(0)
           objects_images.append(obj_image)
-
+      object_names.append("unknown")
       text_snippets = ["a photo of a {}".format(name) for name in object_names]
-      text_snippets.append("a photo of something else")
       # pre-process text
       text = clip.tokenize(text_snippets).to(self.device)
       
@@ -102,15 +130,16 @@ class PCLProcessor:
           # print("Label probs:", ["{0:.10f}".format(i) for i in probs[0]])
           obj_idx = np.argmax(probs[0])
           print(obj_idx)
-          if (obj_idx < len(object_names)) and (obj_idx != len(object_names) - 1):
+          print(probs[0])
+          if (probs[0][obj_idx] > 0.6):
               print("Object {} is {}".format(i, object_names[obj_idx]))
               detected_objects.append((obj_idx, objects_on_table_roi[i]))
       
       for i, detected_object in detected_objects:
           # print("Object {} is {}".format(i, object_names[i]))
           if detected_object is not None:
-              cv2.rectangle(image_np, (detected_object[0][1], detected_object[0][0]), (detected_object[1][1], detected_object[1][0]), (0, 255, 0), 2)
-              cv2.putText(image_np, object_names[i], (detected_object[0][1], detected_object[0][0]), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 2)
+              cv2.rectangle(image_np, (detected_object[0][0], detected_object[0][1]), (detected_object[1][0], detected_object[1][1]), (0, 255, 0), 2)
+              cv2.putText(image_np, object_names[i], (detected_object[0][0], detected_object[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
       cv2.imshow("image", image_np)
       cv2.waitKey(0)
       return detected_objects
@@ -119,6 +148,6 @@ class PCLProcessor:
 if __name__ == '__main__':
   try:
     pcl_processor = PCLProcessor()
-    pcl_processor.find_object(object_names=["cup", "container cover", "tea packet", "headset cover", "computer mouse"])
+    pcl_processor.find_object(object_names=["cup", "bottle"])
   except rospy.ROSInterruptException:
     pass
